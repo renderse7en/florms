729729c8a97e290ff0bae9548d40d57104a5d1d2
Jamin <jamin@se7enet.com>
2019-11-09 08:40:10 -0600

    + Add facade

---
+++ src/FlormsFacade.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Se7enet\Florms;
+
+use Illuminate\Support\Facades\Facade;
+
+class FlormsFacade extends Facade
+{
+    /**
+     * Get the registered name of the component.
+     *
+     * @return string
+     */
+    protected static function getFacadeAccessor()
+    {
+        return 'florm';
+    }
+}
