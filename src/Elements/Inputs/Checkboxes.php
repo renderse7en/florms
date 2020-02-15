59463a7a5a2579f6f9b55069dfd288e9648d7c5a
Jamin <jamin@se7enet.com>
2020-02-15 08:39:04 -0600

    ~ Support options array for each value

--- src/Elements/Inputs/Checkboxes.php
+++ src/Elements/Inputs/Checkboxes.php
@@ -26,7 +26,7 @@ class Checkboxes extends Inputs
      * should be a a simple associative array where each key is the "value" 
      * attribute of the checkbox, and each value is the label text for the 
      * checkbox.
-     *
+     * 
      * A more advanced version should be a 2D array, where each primary key is
      * again the attribute of the checkbox, and each value is itself an
      * associative array of the options to be passed into that checkbox. If the
@@ -87,24 +87,35 @@ class Checkboxes extends Inputs
      * Render an individual checkbox into this group.
      *
      * @param string  $value
-     * @param string  $label
+     * @param mixed   $label
      * @param integer $count
      *
      * @return string
      */
     public function renderOption($value, $label, $count = 0)
     {
+        // Inherit the default settings.
         $id       = $this->getAttribute('id');
         $name     = $this->getAttribute('name');
         $defaults = $this->controlDefaults ?? [];
 
+        // If $label is an array, then we should use that for the base $options.
+        $options = [];
+        if (is_array($label)) {
+            $options = $label;
+            $label = $options['label'] ?? '';
+        }
+
+        // Create a complete options array with our programmatic defaults,
+        // merged with the provided defaults as well as any options that were
+        // specified in the original advanced $label array.
         $options = array_merge([
             'id'        => sprintf('%s-%s', $id, $count),
             'name'      => $name,
             'value'     => $value,
             'label'     => $label,
             'formGroup' => false,
-        ], $defaults);
+        ], $defaults, $options);
 
         $element = $this->getOptionElement($options);
 
